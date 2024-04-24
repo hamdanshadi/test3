a4d6e3e26fd05c665c9301fb60181e2e95850a18
hamdanshadi <shadi.hamdan@gmail.com>
2024-04-24 19:42:33 +0200

    added yo

--- hungry.py
+++ hungry.py
@@ -1,6 +1,6 @@
 print("I am Hungry")
 hungry = input("are you hungry?")
-if hungry == "yes":
+if hungry == "yes" and hungry == "yo":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
